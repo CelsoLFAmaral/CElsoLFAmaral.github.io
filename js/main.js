d3de1fca5fd3d007a49490733c39707411659a0e
Celso Luiz Ferraz do Amaral <32436425+CelsoLFAmaral@users.noreply.github.com>
2019-11-01 19:21:08 -0300

    menu terminado e inserção de pop up competencias

--- js/main.js
+++ js/main.js
@@ -35,3 +35,57 @@ $('a[href*="#"]')
             }
         }
     });
+var header = document.querySelector('.header'),
+    main = document.querySelector('.main'),
+    footer = document.querySelector('.footer'),
+    nav = document.querySelector('.nav'),
+    menu = document.querySelector('#menu'),
+    botao = document.querySelector('.nav__hamburguer'),
+    botaoDesktop = document.querySelector('.header__hamburguer'),
+    botaoClose = document.querySelector('.nav__close'),
+    link = document.querySelectorAll('.nav__link'),
+    competencia = document.querySelectorAll('.sectionCompetencias__tecnicas');
+
+
+botao.addEventListener('click', function () {
+    if (menu.className === "nav__menu") {
+    menu.classList.add("responsive");
+    } else {
+        menu.classList.remove("responsive");
+    }
+});
+
+botaoDesktop.addEventListener('click', function () {
+    nav.classList.add("responsive");
+    header.style.marginLeft = "250px";
+    main.style.marginLeft = "250px";
+    footer.style.marginLeft = "250px";
+    this.style.display = "none";
+
+});
+botaoClose.addEventListener('click', function () {
+    nav.classList.remove("responsive");
+    header.style.marginLeft = "0";
+    main.style.marginLeft = "0";
+    footer.style.marginLeft = "0";
+    botaoDesktop.style.display = "block";
+
+})
+
+link.forEach(element => {
+    element.addEventListener('click',function () {
+        menu.classList.remove("responsive");     
+
+    })
+});
+
+competencias.forEach(element => {
+    element.addEventListener('click',function () {
+        
+    })
+});
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
